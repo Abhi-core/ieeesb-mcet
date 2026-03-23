1f4edb5a1e1cb447fbd6bc0c9328a70b540b40d5
Abhi-core <abijithpb8113@gmail.com>
2026-03-24 00:21:27 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#typing", {
+    strings: ["MCET", "IEEE"],   // Texts that appear after "About"
+    typeSpeed: 100,              // Typing speed (ms per character)
+    backSpeed: 60,               // Backspacing speed
+    backDelay: 1200,             // Pause before erasing
+    loop: true                   // Keep repeating
+  });
+});
--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Typed("#typing", {
-    strings: ["MCET", "IEEE"],   // Texts that appear after "About"
-    typeSpeed: 100,              // Typing speed (ms per character)
-    backSpeed: 60,               // Backspacing speed
-    backDelay: 1200,             // Pause before erasing
-    loop: true                   // Keep repeating
-  });
-});
